1159401d8e78a377daa9a21195521b88e61f72bd
Pavel Yakubovskiy <qubvel@gmail.com>
2019-05-20 15:22:13 +0300

    optimize mAPxx calculation

--- aeronet/criterions/vector.py
+++ aeronet/criterions/vector.py
@@ -11,47 +11,63 @@ def iou(gt_feature, pr_feature):
     return (intersection + 10e-12) / (union + 10e-12)
 
 
-def confusion_matrix(gt_fc, pr_fc, iou_threshold=0.5):
-    tp = 0
-    fn = 0
-
+def collection_iou(gt_fc, pr_fc) -> np.array:
+    
+    scores = []
+    
     for feature in gt_fc:
 
         proposed_features = pr_fc.intersection(feature)
-        iou_scores = [iou(feature, f) for f in proposed_features]
-
-        if len(iou_scores) == 0:
-            fn += 1
-        elif max(iou_scores) >= iou_threshold:
-            tp += 1
+        feature_scores = [iou(feature, f) for f in proposed_features]
+        if feature_scores:
+            max_iou_score = max([iou(feature, f) for f in proposed_features])
         else:
-            fn += 1
+            max_iou_score = 0
+        scores.append(max_iou_score)
+    
+    return np.array(scores)
+        
+
+def confusion_matrix(gt_fc, pr_fc, iou_threshold=0.5):
+    
+    scores = collection_iou(gt_fc, pr_fc)
+    tp = sum(scores > iou_threshold)
     fp = len(pr_fc) - tp
+    fn = len(gt_fc) - tp
+    
     return tp, fp, fn
 
 
-def mAP(gt_fc, pr_fc, iou_threshold=0.5, beta=1):
-    tp, fp, fn = confusion_matrix(gt_fc, pr_fc, iou_threshold=iou_threshold)
-    score = ((1 + beta ** 2) * tp + EPS) / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + EPS)
-    return score
-
-
-def mAP50(gt_fc, pr_fc, beta=1):
-    return mAP(gt_fc, pr_fc, beta=beta, iou_threshold=0.5)
-
-
-def mAP75(gt_fc, pr_fc, beta=1):
-    return mAP(gt_fc, pr_fc, beta=beta, iou_threshold=0.75)
-
-
-def mAPxx(gt_fc, pr_fc, thresholds, beta=1):
-    mAPs = [mAP(gt_fc, pr_fc, beta=beta, iou_threshold=t) for t in thresholds]
+def mAPxx(gt_fc, pr_fc, thresholds, beta=1) -> float:
+    
+    scores = collection_iou(gt_fc, pr_fc)
+    mAPs = []
+    
+    for threshold in thresholds:
+        tp = sum(scores > threshold)
+        fp = len(pr_fc) - tp
+        fn = len(gt_fc) - tp
+        
+        mAP_ = ((1 + beta ** 2) * tp + EPS) / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + EPS)
+        mAPs.append(mAP_)
+        
     return np.mean(mAPs)
 
 
-def mAP5095(gt_fc, pr_fc, beta=1):
+def mAP(gt_fc, pr_fc, iou_threshold=0.5, beta=1) -> float:
+    return mAPxx(gt_fc, pr_fc, thresholds=[iou_threshold], beta=beta)
+
+
+def mAP50(gt_fc, pr_fc, beta=1) -> float:
+    return mAPxx(gt_fc, pr_fc, beta=beta, thresholds=[0.5])
+
+
+def mAP75(gt_fc, pr_fc, beta=1) -> float:
+    return mAPxx(gt_fc, pr_fc, beta=beta, thresholds=[0.75])
+
+def mAP5095(gt_fc, pr_fc, beta=1) -> float:
     thresholds = np.linspace(0.5, 0.95, 10)
-    return mAPxx(gt_fc, pr_fc, thresholds, beta=beta)
+    return mAPxx(gt_fc, pr_fc, beta=beta, thresholds=thresholds)
 
 
 # ============================= Area metrics ==================================
